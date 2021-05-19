9884748c455fb0c2a8c40cfb459c9510f6b63635
Alex Sasov <sasov1993@mail.ru>
2021-05-19 09:37:31 +0300

    signal r проект транспорт

---
+++ WebSRTransport/WebSRTransport/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using WebSRTransport.Hubs;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSRTransport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public ChatController(IHubContext<ChatHub> hubContext) : base()
+        {
+            _hubContext = hubContext;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{msg}")]
+        public async void Send(string msg)
+        {
+            await _hubContext.Clients.All.SendAsync("Send", msg); ;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async void Post([FromBody]string message)
+        {
+            await _hubContext.Clients.Group(nameof(ChatHub)).SendAsync("Send", message);
+        }
+    }
+}
