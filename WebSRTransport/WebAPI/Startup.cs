3b7864cbe37b6fd55bab91a92129bcbb8e50fea0
Alex Sasov <sasov1993@mail.ru>
2021-05-19 09:47:32 +0300

    регистрация сваггера

--- WebSRTransport/WebAPI/Startup.cs
+++ WebSRTransport/WebAPI/Startup.cs
@@ -26,11 +26,23 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmsApi");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,6 +51,11 @@ namespace WebAPI
             {
                 app.UseHsts();
             }
+
+            // подключаем CORS
+            app.UseCors(builder => builder.AllowAnyOrigin()
+                                          .AllowAnyHeader()
+                                          .AllowAnyMethod());
 
             app.UseHttpsRedirection();
             app.UseMvc();
