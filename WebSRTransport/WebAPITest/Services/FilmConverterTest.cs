5ca713f67fdd1b332660c22959357573f74e500e
Alex Sasov <sasov1993@mail.ru>
2021-06-04 00:18:22 +0300

    добавил проект с тестами nunit

---
+++ WebSRTransport/WebAPITest/Services/FilmConverterTest.cs
@@ -0,0 +1,66 @@
+using DB.DBModels;
+using NUnit.Framework;
+using System;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPITest.Services
+{
+    [TestFixture]
+    public class FilmConverterTest
+    {
+        private Lazy<FilmConverter> _converter = new Lazy<FilmConverter>(()=>new FilmConverter());
+
+
+        private Film getFilm(int index)
+        {
+            return new Film()
+            {
+                Id = index,
+                Name = $"Test {index}",
+                Type = new FilmType() { Id = index },
+                Year = 2000 + index,
+                Description = $"Description {index}",
+                Country = $"Country {index}",
+                Timing = index,
+                Budget = 1000 + index,
+                State = FilmState.Active
+            };
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(1)]
+        public void toDBTest(int index)
+        {
+            var domain = getFilm(index);
+            var db = _converter.Value.toDB(domain);
+
+            Assert.AreEqual(domain.Id, db.Id);
+            Assert.AreEqual(domain.Name, db.Name);
+            Assert.AreEqual(domain.Description, db.Descriptions);
+            Assert.AreEqual(domain.Country, db.Country);
+            Assert.AreEqual((int)domain.State, db.Status);
+            Assert.AreEqual(domain.Type.Id, db.TypeId);
+            Assert.AreEqual(domain.Timing, db.Timing);
+            Assert.AreEqual(domain.Budget, db.Budget);
+        }
+
+        [Test]
+        public void toViewTest([Range(0, 10, 2)] int index)
+        {
+            var dmn = getFilm(index);
+            var db = _converter.Value.toDB(dmn);
+            var domain = _converter.Value.toView(db);
+
+            Assert.AreEqual(domain.Id, db.Id);
+            Assert.AreEqual(domain.Name, db.Name);
+            Assert.AreEqual(domain.Description, db.Descriptions);
+            Assert.AreEqual(domain.Country, db.Country);
+            Assert.AreEqual((int)domain.State, db.Status);
+            Assert.AreEqual(domain.Type.Id, db.TypeId);
+            Assert.AreEqual(domain.Timing, db.Timing);
+            Assert.AreEqual(domain.Budget, db.Budget);
+        }
+    }
+}
