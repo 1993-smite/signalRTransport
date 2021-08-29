97d39dc136764c8980600a165b44aba6a0fbc507
Alex Sasov <sasov1993@mail.ru>
2021-08-29 10:00:06 +0300

    поправил форматирование

--- WebSRTransport/FilmsClient/films/src/views/Animate.vue
+++ WebSRTransport/FilmsClient/films/src/views/Animate.vue
@@ -43,14 +43,14 @@
         animation: bounce-in 0.5s reverse;
     }
     @keyframes bounce-in {
-    0% {
-        transform: scale(0);
-    }
-    50% {
-        transform: scale(1.25);
-    }
-    100% {
-        transform: scale(1);
-    }
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.25);
+        }
+        100% {
+            transform: scale(1);
+        }
     }
 </style>
