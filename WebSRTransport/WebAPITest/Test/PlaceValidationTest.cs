0eb377a652c80d30372c1d1c884303bb9f39892e
Alex Sasov <sasov1993@mail.ru>
2021-09-28 21:45:05 +0300

    добавил тесты по новой валидации

---
+++ WebSRTransport/WebAPITest/Test/PlaceValidationTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using WebAPI.Models;
+
+namespace WebAPITest.Test
+{
+    [TestFixture]
+    public class PlaceTest
+    {
+        [Test]
+        public void PlaceValidationTest()
+        {
+            var place = new Place();
+            var validation = new PlaceValidation();
+
+            var result = validation.Validate(place);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(result.Errors.Count, 1);
+        }
+    }
+}
---
+++ WebSRTransport/WebAPITest/Test/WebTaskTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using WebAPI.Models;
+
+namespace WebAPITest.Test
+{
+    [TestFixture]
+    class WebTaskTest
+    {
+        [Test]
+        public void NewWebTaskValidationTest()
+        {
+            var webTask = new WebTask()
+            {
+                Id = 0
+            };
+            var validation = new WebTaskValidation();
+            var result = validation.Validate(webTask);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(result.Errors.Count, 2);
+        }
+
+        [Test]
+        public void WebTaskValidationTest()
+        {
+            var webTask = new WebTask()
+            {
+                Id = 1
+            };
+            var validation = new WebTaskValidation();
+            var result = validation.Validate(webTask);
+
+            Assert.IsFalse(result.IsValid);
+            Assert.AreEqual(result.Errors.Count, 1);
+        }
+    }
+}
