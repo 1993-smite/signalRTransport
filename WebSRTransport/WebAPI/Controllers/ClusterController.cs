ab068100db681df8720a75c382afa7670207a921
Alex Sasov <sasov1993@mail.ru>
2021-11-04 23:26:54 +0300

    добавил методы для типовой кластеризации

--- WebSRTransport/WebAPI/Controllers/ClusterController.cs
+++ WebSRTransport/WebAPI/Controllers/ClusterController.cs
@@ -14,44 +14,39 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ClusterizeController : ControllerBase
     {
-        //[HttpPost]
-        //public ActionResult<Cluster<GeographicPoint>> Post([FromBody] ClusterizeParam param)
-        //{
-        //    var clusterizer = new Clusterization<CartesianPoint>();
-            
-        //    var geographic = new List<GeographicPoint>();
-        //    var xy = new List<CartesianPoint>();
-        //    for(int i = 0; i < param.Places.Length; i++)
-        //    {
-        //        var place = param.Places[i];
-        //        var geographicPoint = new GeographicPoint(i + 1, (double)place.Lat, (double)place.Lon);
-        //        geographic.Add(geographicPoint);
-        //        xy.Add(geographicPoint.toCartesian());
-        //    }
-
-        //    var clusters = clusterizer
-        //        .Clusterize(xy, param.Count);
-        //    var geographicClusters = new List<Cluster<GeographicPoint>>();
-        //    foreach (var cluster in clusters)
-        //    {
-        //        geographicClusters.Add(new Cluster<GeographicPoint>()
-        //        {
-        //            Items = cluster.Items.Select(x => geographic.FirstOrDefault(p => p.Id == x.Id)).ToList()
-        //        });
-        //    }
-
-        //    var res = geographicClusters.Select(x => new ClusterView(x));
-        //    return Ok(res);
-        //}
+        private IEnumerable<ClusterView> clusterize(Clusterization<GeographicPoint> clusterizer, ClusterizeParam param)
+        {
+            var clusters = clusterizer
+                .Clusterize(param.Places.Select(x => new GeographicPoint(0, (double)x.Lat, (double)x.Lon)), param.Count);
+            return clusters.Select(x => new ClusterView(x));
+        }
 
         [HttpPost]
         public ActionResult<Cluster<GeographicPoint>> AccordKMeans([FromBody] ClusterizeParam param)
         {
-            var clusterizer = new Clusterization<GeographicPoint>();
-            var clusters = clusterizer
-                .Clusterize(param.Places.Select(x=>new GeographicPoint(0,(double)x.Lat, (double)x.Lon)), param.Count);
-            var res = clusters.Select(x => new ClusterView(x));
-            return Ok(res);
+            var clusterizer = new Clusterization<GeographicPoint>(ClusterizationMode.Kmeans);
+            return Ok(clusterize(clusterizer, param));
+        }
+
+        [HttpPost]
+        public ActionResult<Cluster<GeographicPoint>> AccordBinarySplit([FromBody] ClusterizeParam param)
+        {
+            var clusterizer = new Clusterization<GeographicPoint>(ClusterizationMode.BinarySplit);
+            return Ok(clusterize(clusterizer, param));
+        }
+
+        [HttpPost]
+        public ActionResult<Cluster<GeographicPoint>> AccordMeanSplit([FromBody] ClusterizeParam param)
+        {
+            var clusterizer = new Clusterization<GeographicPoint>(ClusterizationMode.MeanSplit);
+            return Ok(clusterize(clusterizer, param));
+        }
+
+        [HttpPost]
+        public ActionResult<Cluster<GeographicPoint>> AccordGausian([FromBody] ClusterizeParam param)
+        {
+            var clusterizer = new Clusterization<GeographicPoint>(ClusterizationMode.Gausian);
+            return Ok(clusterize(clusterizer, param));
         }
     }
 }
