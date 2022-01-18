d1efae3474cfca0153256a772698af9cd1b9dda6
Alex Sasov <sasov1993@mail.ru>
2022-01-18 21:14:41 +0300

    добавил общие рельсы для команды

---
+++ WebSRTransport/Common.lib.cqrs.commands/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Common.lib.cqrs.commands.Commands.Answer;
+
+namespace Common.lib.cqrs.commands.Commands
+{
+    public class Command<TModel,TOutModel>: IRequest<Answer<TOutModel>>
+    {
+        public TModel Model;
+
+        public Command(TModel model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ WebSRTransport/Common.lib.cqrs.commands/Commands/CommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using DB.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Common.lib.cqrs.commands.Commands.Answer;
+
+namespace Common.lib.cqrs.commands.Commands
+{
+    public class CommandHandler<TModel, TFilter, TOutModel>
+        : IRequestHandler<Command<TFilter, TOutModel>, Answer<TOutModel>>
+    {
+        protected readonly IMediator _mediator;
+        protected readonly CommonRepository<TModel, TFilter> _repository;
+
+        public CommandHandler(IMediator mediator, CommonRepository<TModel, TFilter> repository)
+        {
+            _mediator = mediator;
+            _repository = repository;
+        }
+        
+        public virtual Task<Answer<TOutModel>> Handle(Command<TFilter, TOutModel> request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
