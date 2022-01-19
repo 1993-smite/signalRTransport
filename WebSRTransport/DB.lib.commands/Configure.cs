16b39c4c0dcf8d8c8eee168d198622ed797bb842
Alex Sasov <sasov1993@mail.ru>
2022-01-19 20:55:51 +0300

    добавил класс для конфигурации

---
+++ WebSRTransport/DB.lib.commands/Configure.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace DB.lib.commands
+{
+    public static class Configure
+    {
+        public static void AddAppMediatR(this IServiceCollection services)
+        {
+            var assembly = typeof(Configure).Assembly;
+
+            services.AddMediatR(assembly);
+        }
+    }
+}
