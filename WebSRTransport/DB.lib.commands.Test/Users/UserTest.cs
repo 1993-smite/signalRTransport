8c6fd837d896357e5b166cfb6019fc7a4c025e35
Alex Sasov <sasov1993@mail.ru>
2022-01-29 00:26:12 +0300

    тест для сqrs

---
+++ WebSRTransport/DB.lib.commands.Test/Users/UserTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace DB.lib.commands.Test
+{
+    public class Tests
+    {
+        IServiceCollection ServiceProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void UserSaveTest()
+        {
+            
+
+            Assert.Pass();
+        }
+    }
+}
