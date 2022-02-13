eb6044ebfbfa041ada81d841ca08c1e6283add48
Alex Sasov <sasov1993@mail.ru>
2022-02-13 20:59:44 +0300

    simple rabbit listener

---
+++ WebSRTransport/RabbitListener/Program.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using RabbitCore.Core.Listeners;
+using System;
+
+namespace RabbitListener
+{
+    class Program
+    {
+        public static NLog.Logger Logger
+            = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            var listener = new RabbitMqListener("MyQueue");
+            listener.Listen(GetMessage);
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static void GetMessage(string msg)
+        {
+            Logger.Debug(msg);
+            Console.WriteLine(msg);
+        }
+    }
+}
